10118dad02a3a07e900e3b91c79d45cc9c293368
Syu <game880818@gmail.com>
2025-12-31 20:38:27 +0900

    登陸頁面驗證及提交

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,17 +3,32 @@ import { Card, Form, Input, Button } from 'antd'
 import logo from '@/assets/logo.png'
 
 const Login = () => {
+  const onFinish = (values) => {
+    console.log(values)
+  }
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
-        {/* 登录表单 */}
-        <Form>
-          <Form.Item>
-            <Input size="large" placeholder="请输入手机号" />
+        {/* 登录表单  */}
+        <Form validateTrigger='onBlur' onFinish={onFinish}>
+          {/* 設置驗證規則 rules */}
+          <Form.Item
+            name='phone'
+            rules={
+              [
+                { required: true, message: '請輸入手機號碼' },
+                { pattern: /^09\d{8}$/, message: '請輸入手機號碼格式正確' }
+              ]
+            }
+          >
+            <Input size="large" placeholder="請輸入手機號碼" />
           </Form.Item>
-          <Form.Item>
-            <Input size="large" placeholder="请输入验证码" />
+          <Form.Item
+            name='code'
+            rules={[{ required: true, message: '請輸入驗證碼' }]}
+          >
+            <Input size="large" placeholder="請输入驗證碼" />
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit" size="large" block>
